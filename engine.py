78b6c06285d5b143127536009c02ccf61f76919d
GabiMinecraft02 <gabrielmeresse09@gmail.com>
2026-02-03 18:55:57 +0100

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,31 @@
+from flask import request, session, redirect, url_for, render_template
+import os
+
+password = os.getenv("PASSWORD")
+
+@app_route(/)
+def home():
+  return render_template("index.html")
+
+@app_route(/adminlogin)
+def home():
+  return render_template("AdminLogin.html")
+
+@app.route("/Admin", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        password = request.form.get("password")
+
+        if password == "monmotdepasse":
+            session["password"] = password
+            return render_template("Admin.html")
+        else:
+            return redirect("/adminlogin")
+
+    if "password" not in session:
+        return redirect("/adminlogin")
+
+    return render_template("Admin.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
